4fb11886a0327519d85c3822461939738afdabb1
Frans <lebepef@gmail.com>
2023-10-20 06:23:18 -0700

    my shell

---
+++ _atoi.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is in interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if in interactive mode, 0 otherwise
+ */
+
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if the charact is a delimiter
+ * @c: the char to check
+ * @delim: the delimiter string
+ * Return: 1 if true, 0 if not
+ */
+
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if(*delim++ ==c)
+			return (1);
+	return (0)
+}
+
+/**
+ * _isalpha - checks for the alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >='a' && <= 'z') || (c >= 'A' && C <= 'Z'))
+		return (1);
+	else
+		return (0)
+}
+
+/**
+ * _atoi - convert a string to a interger
+ *@s: a string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sighn == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
